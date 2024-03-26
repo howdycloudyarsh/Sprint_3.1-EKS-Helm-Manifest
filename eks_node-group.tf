f7b90ba5c5fda4c0f8f11f8e7fc5887558149503
arshad <ahmad.arshad2009@gmail.com>
2024-03-26 15:38:31 +0530

    adding files

---
+++ eks_node-group.tf
@@ -0,0 +1,49 @@
+# Create the EC2 instance
+resource "aws_instance" "kubectl-server" {
+  ami                         = "ami-080e1f13689e07408"
+  key_name                    = "terra2"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public-1.id
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "kubectl-server"
+  }
+
+}
+
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "node"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  capacity_type   = "ON_DEMAND"
+  disk_size       = "20"
+  instance_types  = ["t3.small"]
+
+  remote_access {
+    ec2_ssh_key               = "terra2"
+    source_security_group_ids = [aws_security_group.allow_tls.id]
+  }
+
+  labels = tomap({ env = "dev" })
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    #aws_subnet.pub_sub1,
+    #aws_subnet.pub_sub2,
+  ]
+}
